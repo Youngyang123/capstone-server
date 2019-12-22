8299fdc6908bb5e65739a29b7a5b7cd851c75e57
young <youngysm@163.com>
2019-12-23 02:03:42 +0800

    dfd

--- src/main/java/cn/yg/capstoneserver/mapper/articalTagRefMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.yg.capstoneserver.mapper;
-
-import cn.yg.capstoneserver.entity.ArticalTagRef;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface ArticalTagRefMapper extends Mapper<ArticalTagRef> {
-}
